6b2ec72cc13e8794fd47b6c459c6bc3190391a6c
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-03-28 23:23:45 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240328/길이 단위 환산하기/convert-length-units.cpp
+++ 240328/길이 단위 환산하기/convert-length-units.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
     double ft = 30.48;
-    double a;
-    cin >> a;
+    double n;
+    
+    cin >> n;
 
     cout << fixed;
     cout.precision(1);
-    cout << a * ft;
+    cout << n * ft;
 
     return 0;
 }
