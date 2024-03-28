c82f82e52d3b44a242e59e109705d1b134abf355
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-03-28 23:54:11 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int x, y, z;
+    char b;
+
+    cin >> z >> b >> x >> b >> y;
+    cout << "010" << b << y << b << x;
+
+    return 0;
+}
