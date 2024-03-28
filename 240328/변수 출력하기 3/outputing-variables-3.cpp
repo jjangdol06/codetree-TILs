5e48fec440ffa7b751245a0b57b193280993144f
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-03-28 22:05:05 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2;
+    char c = 'C';
+    string s = "->";
+
+    cout << a << s << b << s << c;
+    
+    return 0;
+}
