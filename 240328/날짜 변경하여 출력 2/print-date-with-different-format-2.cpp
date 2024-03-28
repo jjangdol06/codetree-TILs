9b95421eb81f028bbe237ab9d5b93e225c18a389
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-03-28 23:41:10 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int m , d, y;
+    char c;
+
+    cin >> m >> c >> d >> c >> y;
+    c = '.';
+    cout << y << c << m << c << d;
+
+    return 0;
+}
