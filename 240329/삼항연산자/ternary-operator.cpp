eaebebeae8fae9eb9f5f97e0282cf6918ac1f6eb
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-03-29 23:28:16 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int score;
+
+    cin >> score;
+
+    if(score == 100)
+         cout << "pass";
+    else
+         cout << "failure";
+
+    return 0;
+}
