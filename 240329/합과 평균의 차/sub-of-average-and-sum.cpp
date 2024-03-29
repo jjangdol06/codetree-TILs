b069962aec03b8a5e369f287c0ed8b73fe4e8e26
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-03-29 23:09:28 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    a += b;
+    a += c;
+
+    cout << a << endl;
+    cout << a / 3 << endl; 
+    cout << a * 2 / 3;
+
+    return 0;
+}
