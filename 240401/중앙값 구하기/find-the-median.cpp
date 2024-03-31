e1d9eef9ec12efdf374771078bdb99def1370df3
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-01 00:20:12 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240401/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c, temp;
+
+    cin >> a >> b >> c;
+
+    if(a > b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    if(b > c){
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    cout << b;
+
+    return 0;
+}
