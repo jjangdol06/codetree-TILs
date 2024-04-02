30ba2e7bb9ce8b24d6654964065ac916a66f7c63
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-02 23:00:42 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, cnt1 = 0, cnt2 = 0;
+
+    for(int i = 0; i < 10; i++){
+        cin >> n;
+
+        if(n % 3 == 0)
+            cnt1++;
+        if(n % 5 == 0)
+            cnt2++;
+    }
+
+    cout << cnt1 << ' ' << cnt2;
+
+    return 0;
+}
