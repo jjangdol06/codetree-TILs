ee689115e6e3f396eca5cc16bf11db88a0572852
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-03 22:12:13 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240403/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, cnt = 0;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        if(i % 4 == 0)
+            cnt++;
+        if(i % 100 == 0 && i % 400 != 0)
+            cnt--;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
