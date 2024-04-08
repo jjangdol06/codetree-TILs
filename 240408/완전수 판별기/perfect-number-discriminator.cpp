eed5c466b271b6a1feb3c290e1dfaa560e62b27a
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-08 22:51:08 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, sum = 0;
+
+    for (int i = 1; i <= a; i++) {
+		if (a % i == 0) {
+			sum += i;
+		}
+	}
+
+    if(a == sum)
+        cout << 'P';
+    else
+        cout << 'N';
+
+    return 0;
+}
