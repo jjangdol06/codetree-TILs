43ff049fe900a64ac5263e623d005ad25d4dfc9f
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-17 20:57:33 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240417/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n, temp, cnt = 0;
+
+    cin >> n;
+
+    temp = n;
+    for(int i = 1; i <= n; i++){
+        temp /= i;
+        cnt++;
+        if(temp < 1)
+            break;
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
