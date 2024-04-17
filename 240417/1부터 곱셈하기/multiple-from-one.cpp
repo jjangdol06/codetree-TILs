dcbba8e37abda6fc2af8152a14531ae5c7d3fe1d
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-17 20:41:47 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240417/1부터 곱셈하기/multiple-from-one.cpp
+++ 240417/1부터 곱셈하기/multiple-from-one.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main() {
 
-    int n, res = 1;
+    int n, prod = 1;
 
     cin >> n;
 
     for(int i = 1; i <= 10; i++){
-        res *= i;
-        if(res >= n){
+        prod *= i;
+        if(prod >= n){
             cout << i;
             break;
         }
     }
-    
+
     return 0;
 }
