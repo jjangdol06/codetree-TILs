5f611c3f710ace64d40519da34d47913a325bd7e
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-20 21:16:36 +0900

    Title: 3n + 1 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240420/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, cnt = 0;
+    cin >> n;
+
+    while(1){
+        if(n == 1){
+            cout << cnt;
+            break;
+        }
+
+        if(n % 2 == 0)
+            n /= 2;
+        else
+            n = n * 3 + 1;
+        
+        cnt++;
+    }
+
+    return 0;
+}
