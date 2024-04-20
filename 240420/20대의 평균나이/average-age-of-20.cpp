9d305a45ba50db897c1f1eaa88c438c4d9abb006
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-20 20:47:07 +0900

    Title: 20대의 평균나이, Time: 1091ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240420/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int cnt = 0;
+    double sum = 0;
+
+    while(1){
+        int age;
+        cin >> age;
+        
+        if(age >= 30){
+            cout << fixed;
+            cout.precision(2);
+            cout << sum / cnt;
+            break;
+        }
+
+        sum += age;
+        cnt++;
+    }
+    
+    return 0;
+}
