48c0ceaf478031b0473774a24590bb0c744265b0
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-20 21:50:28 +0900

    Title: ab 사이에 있는 c, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240420/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    for(int i = a; i <= b; i++){
+        if(i % c == 0){
+            cout << "YES";
+            break;
+        }
+
+        if(i == b)
+            cout << "NO";
+    }
+    
+    return 0;
+}
