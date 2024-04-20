8e9fde8017d1e8e19f31667bb16e36a323d1e6c7
Jiyeon Kang <43868369+jjangdol06@users.noreply.github.com>
2024-04-20 23:06:29 +0900

    Title: 학점 계산기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240420/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    double avg;
+    cin >> n;
+
+    double arr[n];    
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        avg += arr[i];
+    }
+
+    avg /= 4;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << avg << endl;
+    
+    if(avg >= 4.0)
+        cout << "Perfect";
+    else if(avg >= 3.0)
+        cout << "Good"; 
+    else
+        cout << "Poor";
+
+    return 0;
+}
